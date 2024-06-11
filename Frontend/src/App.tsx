8e35144da5fff8523463f02c73d98ea2549329b1
M Prasaanth <prasaanth2208@gmail.com>
2024-06-11 15:57:06 +0800

    landingpage

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+import './App.css'
+import LandingPage from './pages/LandingPage'
+import NavBar from './components/NavBar'
+
+function App() {
+
+
+  return (
+    <>
+    <NavBar></NavBar>
+      <LandingPage></LandingPage>    
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./css/NavBar.module.css"
+
+const NavBar = () => {
+    return (
+        <div className={styles.navBar}>
+            <button>Theme</button>
+        </div>
+    );
+};
+
+export default NavBar;
