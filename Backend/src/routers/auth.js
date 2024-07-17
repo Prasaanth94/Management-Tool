7dc133f52b010697b2ed55bfb00adef18eeb5f3a
M Prasaanth <prasaanth2208@gmail.com>
2024-07-17 18:54:45 +0800

    validators

--- Backend/src/routers/auth.js
+++ Backend/src/routers/auth.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { registerUser } = require("../controllers/auth");
+const { validateRegistrationData } = require("../validators/auth");
+const { errorCheck } = require("../validators/errorCheck");
+
 
 
 const router = express.Router();
 
-router.post("/regiser", registerUser);
-
+router.post("/register", validateRegistrationData, errorCheck, registerUser);
 
 module.exports = router;
